823fc6431e1c2fd119ec5ca2c8190320df6faedc
Vigneshwaran J <vigneshwaran.j@jmangroup.com>
2022-12-12 15:07:33 +0530

    'testting'

---
+++ run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Show location of local install of dbt
+echo $(which dbt)
+
+# Show version and installed adapters
+dbt --version
+
+# Set the profile
+cd integration_tests
+cp ci/sample.profiles.yml profiles.yml
+export DBT_PROFILES_DIR=.
+
+# Show the location of the profiles directory and test the connection
+dbt debug
+
+dbt deps 
+dbt seed --full-refresh
+dbt run
+dbt test

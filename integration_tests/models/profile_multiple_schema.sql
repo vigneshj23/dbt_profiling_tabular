846d3753ec3f6a44ea08540b2533677f1c9daafe
Vigneshwaran J <vigneshwaran.j@jmangroup.com>
2022-12-23 11:25:01 +0530

    argument alignement

--- integration_tests/models/profile_multiple_schema.sql
+++ integration_tests/models/profile_multiple_schema.sql
@@ -1,3 +1,3 @@
 
 
-{{ dbt_profiling_tabular.data_profiling('profiling_test',['integration_tests_customer_detail','integration_tests_order_detail'],'profiling_test','profiling','data_profile_table1')}}
+{{ dbt_profiling_tabular.data_profiling('profiling_test','profiling','data_profile_table1','profiling_test',['integration_tests_customer_detail','integration_tests_order_detail'])}}

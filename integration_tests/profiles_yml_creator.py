495265b628ece28f7745a6aa739d04dcdf12a72b
Vigneshwaran J <vigneshwaran.j@jmangroup.com>
2022-12-26 21:35:32 +0530

    test

--- integration_tests/profiles_yml_creator.py
+++ integration_tests/profiles_yml_creator.py
@@ -1,11 +1,17 @@
+#import required package
 from dotenv import dotenv_values
+
+# Read the env variables
 config=dotenv_values(".env")
+
+# Read the sample.profiles.yml file
 f = open('ci/sample.profiles.yml','r',encoding='utf-8')
 data =f.read()
 f.close()
 
 data=data.replace('$','')
 
+# Writing the actual profile.yml file
 final =open('profiles.yml','w',encoding='utf-8')
 final.write(data.format(**config))
 final.close()

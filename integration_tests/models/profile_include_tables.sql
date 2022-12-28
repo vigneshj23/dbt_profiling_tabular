842bc62526d1a8998a54d559f160845865dd51d1
Vigneshwaran J <vigneshwaran.j@jmangroup.com>
2022-12-28 15:37:49 +0530

    changes in order

--- integration_tests/models/profile_include_tables.sql
+++ integration_tests/models/profile_include_tables.sql
@@ -5,5 +5,5 @@
                                         , target_schema='profiling'
                                         , target_table='data_profile_multi_schema'
                                         , source_database='profiling_test'
-                                        , source_schema=[''integration_tests_customer_detail', ''integration_tests_order_detail']
+                                        , source_schema=['integration_tests_customer_detail', 'integration_tests_order_detail']
                                         , include_tables=['customer', 'orders']) }}
--- integration_tests/models/profile_multiple_schema.sql
+++ integration_tests/models/profile_multiple_schema.sql
@@ -4,4 +4,4 @@
                                         , target_schema='profiling'
                                         , target_table='data_profile_multi_schema'
                                         , source_database='profiling_test'
-                                        , source_schema=[''integration_tests_customer_detail', ''integration_tests_order_detail']) }}
+                                        , source_schema=['integration_tests_customer_detail', 'integration_tests_order_detail']) }}

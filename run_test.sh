0e9d83c5b0cfdcb94d26fb2e4241f446fbb434b3
Vigneshwaran J <vigneshwaran.j@jmangroup.com>
2022-12-31 22:06:03 +0530

    env setup

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,8 @@
 cd integration_tests
 echo $ENV_FILE| sed 's/ /\n/g' > .env
+cat .env
 python profiles_yml_creator.py
+cat profiles.yml
 dbt debug --target $1
 dbt deps --target $1
 dbt seed --target $1 --full-refresh
